fd544e80680fe4ae28ec76dab72894e8645da25a
Kate Reading <katereading@asana.com>
2020-01-26 11:32:16 -0800

    Added explore edge options

---
+++ 2A-L2/explore_edge_options.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+# Apply a Gaussian filter to remove noise
+img = cv2.imread('images/fall-leaves.png')
+cv2.imshow('Img', img)
+
+# TODO: Create a Gaussian filter
+
+# TODO: Apply it, specifying an edge parameter (try different parameters)
